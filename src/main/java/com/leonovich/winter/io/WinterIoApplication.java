9ef9f64fd9de4769ecdce5ead7931ee2db52e582
CyberAlexander <taurum13@gmail.com>
2021-12-11 11:58:31 +0300

    Initial commit

---
+++ src/main/java/com/leonovich/winter/io/WinterIoApplication.java
@@ -0,0 +1,13 @@
+package com.leonovich.winter.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinterIoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leonovich/winter/io/WinterIoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonovich.winter.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterIoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
