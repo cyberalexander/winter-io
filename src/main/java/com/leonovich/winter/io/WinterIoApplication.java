5c07671d2db6e1f039992332e34db24a07ba0386
CyberAlexander <taurum13@gmail.com>
2022-01-01 11:48:41 +0300

    Added logging to main class

--- src/main/java/com/leonovich/winter/io/WinterIoApplication.java
+++ src/main/java/com/leonovich/winter/io/WinterIoApplication.java
@@ -1,12 +1,15 @@
 package com.leonovich.winter.io;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Log4j2
 @SpringBootApplication
 public class WinterIoApplication {
 
     public static void main(final String... args) {
         SpringApplication.run(WinterIoApplication.class, args);
+        log.info("{} started!", WinterIoApplication.class.getSimpleName());
     }
 }
