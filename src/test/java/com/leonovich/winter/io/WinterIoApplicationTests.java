500441a8cc3a19ad04dab26a10660f59de805bc5
CyberAlexander <taurum13@gmail.com>
2022-02-13 13:33:30 +0300

    Added test to check @Value annotation

--- src/test/java/com/leonovich/winter/io/WinterIoApplicationTests.java
+++ src/test/java/com/leonovich/winter/io/WinterIoApplicationTests.java
@@ -1,13 +1,25 @@
 package com.leonovich.winter.io;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class WinterIoApplicationTests {
+
+    @Value("${application.some.parameter}")
+    private String someParameter;
 
     @Test
     void contextLoads() {
         //Default unit test verifying if Spring Boot Context initialization successful
+    }
+
+    @Test
+    void testParameterValueTakenFromTestProperties() {
+        Assertions.assertEquals("some test value", someParameter);
     }
 }
